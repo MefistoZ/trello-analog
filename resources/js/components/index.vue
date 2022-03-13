9d9a8662f756912b775457b6865441273d96131d
Igor_Bashuk <igor@redchameleon.com.ua>
2022-03-13 17:47:43 +0200

    init commit

---
+++ resources/js/components/index.vue
@@ -0,0 +1,15 @@
+<template>
+        <div class="container">
+            <h1>App</h1>
+        </div>
+</template>
+
+<script>
+export default {
+    name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
