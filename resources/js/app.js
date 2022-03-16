7f735a8ea36a8c92f1c5abe1ef281c473e80aff9
Igor_Bashuk <igor@redchameleon.com.ua>
2022-03-16 15:06:17 +0200

    feat: test vue-route

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,35 @@
 require('./bootstrap');
 
-import Vue from 'vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+// window.Vue = require('vue').default;
+
+Vue.use(VueRouter)
+
+import Home from "./components/Home";
+import About from "./components/About";
+
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'about',
+        component: About
+    }
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    routes
+})
 
 Vue.component('app', require('./components/index').default)
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    router
 })
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/{any}', [\App\Http\Controllers\SpaController::class, 'index'])->where('any', '.*')->name('index');
