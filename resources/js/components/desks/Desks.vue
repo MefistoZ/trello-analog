d5b5eb58f0cc2043628c5234205cce9be1c80545
Igor_Bashuk <igor@redchameleon.com.ua>
2022-03-16 17:46:17 +0200

    feat: add data to vue component

--- resources/js/components/desks/Desks.vue
+++ resources/js/components/desks/Desks.vue
@@ -1,12 +1,31 @@
 <template>
-<div class="container">
+<div>
     <h1>Доски</h1>
+    <div class="row">
+        <div class="col-lg-4">
+            <div class="card mt-3" style="width: 18rem;">
+                <a href="#" class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                </a>
+            </div>
+        </div>
+    </div>
 </div>
 </template>
 
 <script>
 export default {
-    name: "Desks"
+    data() {
+        return {
+            desks: []
+        }
+    },
+    mounted() {
+        axios.get('/api/V1/desks')
+        .then(response => {
+            this.desks =  response.data.data
+        })
+    }
 }
 </script>
 
--- resources/js/components/index.vue
+++ resources/js/components/index.vue
@@ -1,9 +1,17 @@
 <template>
         <div class="container">
-            <p>
-                <router-link :to="{name: 'home'}">Home</router-link>
-                <router-link :to="{name: 'about'}">About</router-link>
-            </p>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <router-link class="nav-item nav-link active" :to="{name: 'home'}">Главная</router-link>
+                        <router-link class="nav-item nav-link active" :to="{name: 'desks'}">Доски</router-link>
+                    </div>
+                </div>
+            </nav>
             <div>
                 <router-view></router-view>
             </div>
