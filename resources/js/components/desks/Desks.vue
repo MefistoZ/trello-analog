8700828904f24dbaf743377fe5e479fa7d196d74
Igor_Bashuk <igor@redchameleon.com.ua>
2022-03-17 12:35:00 +0200

    feat: show desks from DB

--- resources/js/components/desks/Desks.vue
+++ resources/js/components/desks/Desks.vue
@@ -2,13 +2,19 @@
 <div>
     <h1>Доски</h1>
     <div class="row">
-        <div class="col-lg-4">
+        <div class="col-lg-4" v-for="desk in desks">
             <div class="card mt-3" style="width: 18rem;">
                 <a href="#" class="card-body">
-                    <h5 class="card-title">Card title</h5>
+                    <h5 class="card-title">{{ desk.name }}</h5>
                 </a>
             </div>
         </div>
+    </div>
+    <div class="alert alert-danger" role="alert" v-if="errored">
+        Ошибка загрузки данных!
+    </div>
+    <div class="spinner-grow text-primary" role="status" v-if="loading">
+        <span class="sr-only">Loading...</span>
     </div>
 </div>
 </template>
@@ -17,13 +23,22 @@
 export default {
     data() {
         return {
-            desks: []
+            desks: [],
+            errored: false,
+            loading: true,
         }
     },
     mounted() {
-        axios.get('/api/V1/desks')
+        axios.get('/api/v1/desks')
         .then(response => {
             this.desks =  response.data.data
+        })
+        .catch(error => {
+            console.log(error)
+            this.errored = true;
+        })
+        .finally(() => {
+            this.loading = false;
         })
     }
 }
